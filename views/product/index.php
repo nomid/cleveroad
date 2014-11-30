f0d31c557fd9703d78a1c603f3f04c5a5711da64
dmitriy <nomid21@gmail.com>
2014-11-30 15:47:08 +0200

    if no products

--- views/product/index.php
+++ views/product/index.php
@@ -11,8 +11,13 @@ use yii\widgets\LinkPager;
 $this->title = 'Products';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<?php if(count($products)): ?>
 <?= $this->render('_table',[
     'products'  => $products,
     'pages'     => $pages,
     'my'        => false,
 ]);?>
+<?php else: ?>
+    <p>No products :(</p>
+    <p><a class="btn btn-lg btn-success" href="/product/new">Add products</a></p>
+<?php endif; ?>
